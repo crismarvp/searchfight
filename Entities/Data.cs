0814d90ff922865373df389397ca48129f2a6f01
Cristian Vidal <cristian.vidal@bairesdev.com>
2021-04-25 22:46:28 -0500

    Adding some clases

---
+++ Entities/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace searchfight2
+{
+    public class Data
+    {
+        public string TextSearched { get; set; }
+        public Int64 GoogleResult { get; set; }
+        public Int64 AzureResult { get; set; }
+    }
+}
---
+++ Entities/GoogleData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace searchfight2.Entities
+{
+    public class GoogleData
+    {
+        public Queries queries { get; set; }
+    }
+    public class Request
+    {
+        public string totalResults { get; set; }
+    }
+
+    public class Queries
+    {
+        public List<Request> request { get; set; }
+    }
+
+
+
+
+}
